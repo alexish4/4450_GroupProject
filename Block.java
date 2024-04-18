bf496e593bb4693580079eb7e5dffe4a22b91c84
Megan <mjoo@cpp.edu>
2024-04-18 16:50:18 -0700

    created block class and basic chunk class

---
+++ Block.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package groupproject;
+
+/**
+ *
+ * @author alexhernandez, ethan, megan
+ */
+public class Block {
+    private boolean isActive;
+    private BlockType Type;
+    private float x,y,z;
+    
+    public enum BlockType {
+        BlockType_Grass(0),
+        BlockType_Sand(1),
+        BlockType_Water(2),
+        BlockType_Dirt(3),
+        BlockType_Stone(4),
+        BlockType_Bedrock(5); 
+        private int BlockID;
+
+    
+        BlockType(int i) {
+            BlockID = i;
+        }
+        
+        public int getID() {
+            return BlockID;
+        }
+        
+        public void setID(int id) {
+            BlockID = id;
+        }
+    }
+    
+    //Constructor
+    public Block(BlockType type) {
+        Type = type;
+    }
+    
+    public void setCoords(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+    
+    public int getID() {
+        return Type.getID();
+    }
+    
+}
